402b29321e34de917553e207d999b82c38c8a446
Andreas Müller <andreas.mueller.4@campus.tu-berlin.de>
2018-01-16 13:19:40 +0100

    added: comments

--- fnn-tagger/tagger.py
+++ fnn-tagger/tagger.py
@@ -12,36 +12,44 @@ import tensorflow as tf
 class Tagger:
 	"""A tagger class that trains a Feed-forward Neural Network when instantiated"""
 
-	def __init__(self, training_file_path, vocab_size, n_past_words, embedding_size, n_pos_tags):
+	def __init__(self, training_file_path, vocab_size, n_past_words, embedding_size, h_size, n_pos_tags):
 		""" Takes in the file path to a training file and returns a Tagger object that is able to train and tag sentences"""
 
 		sys.stdout.write('Initializing model ...\n')
 
+		# set path to the training data
 		self.training_file_path = training_file_path
 
+		# initialize input word vectors. None: "variable size"
 		self.input_x = tf.placeholder(tf.int32, [None, n_past_words + 1], name="input_x")
+		# initialize input lables (tags)
 		self.input_y = tf.placeholder(tf.int64, [None], name="input_y")
+		# initialize an embedding matrix with random truncated normal values
 		self.embedding_matrix = tf.Variable(tf.truncated_normal([vocab_size, embedding_size], stddev=0.1))
 
-		# scope: model
+		# build word matrix out of a lookup in the embedding matrix for the input words
 		self.word_matrix = tf.nn.embedding_lookup(self.embedding_matrix, self.input_x)
-		# stack the rows
-		# -1: "figure out the right size" (accounts for variable batch size)
+		# stack the rows to create one vector. -1: "figure out the right size" (accounts for variable batch size)
 		self.feature_vector = tf.reshape(self.word_matrix, [-1, (n_past_words + 1) * embedding_size])
 
-		# one hidden layer
+		# get size of the feature vector
 		feature_vector_size = int(self.feature_vector.shape[1])
-		h_size = 100
+		# initialize weights between input layer and hidden layer
 		w1 = tf.Variable(tf.truncated_normal([feature_vector_size, h_size], stddev=0.1))
+		# compute rectified linear activation function on hidden layer
 		self.h = tf.nn.relu(tf.matmul(self.feature_vector, w1))
+		# initialize weights between hidden layer and output layer
 		self.w2 = tf.Variable(tf.truncated_normal([h_size, n_pos_tags], stddev=0.1))
+		# compute the logits for the output layer with shape [?, n_pos_tags]
 		self.logits = tf.matmul(self.h, self.w2)
+		# compute the loss
 		self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.input_y, logits=self.logits))
 
-		# scope accuracy
-		# logits has shape [?, n_pos_tags]
+		# reduce the logits output with the largest value to the output layer size = predictions 
 		self.predictions = tf.argmax(self.logits, axis=1, name='predictions')
+		# get the correct predictions by comparing to the given labels
 		correct_prediction = tf.equal(self.predictions, self.input_y)
+		# compute the overall accuracy
 		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
@@ -59,6 +67,9 @@ class Tagger:
 
 
 # The default tagger
-t = Tagger(training_file_path='test/path', vocab_size=50000, n_past_words=3, embedding_size=100, n_pos_tags=20)
-t.train()
-t.tag('test sentence\n')
+t = Tagger(training_file_path='test/path', vocab_size=50000, n_past_words=3, embedding_size=100, h_size=100, n_pos_tags=20)
+
+# only execute when script is directly called
+if __name__ == "__main__":
+	t.train()
+	t.tag('test sentence\n')
