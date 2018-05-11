4562d378dce64b4cb27a97403de49a8f5284e425
Andreas Müller <andreas.mueller.4@campus.tu-berlin.de>
2018-05-12 01:36:23 +0200

    updated: training script

--- nn-tagger/training.sh
+++ nn-tagger/training.sh
@@ -4,8 +4,8 @@
 LANG=de_DE.UTF-8
 
 # FNN training | labels: fnn-p-e-s-n
-printf "# FNN MODEL TRAINING\n"
-mkdir storage
+# printf "# FNN MODEL TRAINING\n"
+# mkdir storage
 
 # printf "\n## N Past Words:\n"
 # for p in {0..12}; do
@@ -63,41 +63,41 @@ mkdir storage
 # printf "# RNN MODEL TRAINING\n"
 # mkdir storage
 
-# printf "\n## N Timesteps:\n"
-# for t in {1..4}; do
-#     if [ ! -d "storage/rnn-$t-50-1" ]; then
-#         mkdir storage/rnn-$t-50-1
-#         { time python tagger.py --train data/training.corpus -t $t -s 50 -n 1 ; } > storage/rnn-$t-50-1/training.txt 2>&1
-#         { python tagger.py --evaluate data/evaluation_known.txt -t $t -s 50 -n 1 ; } > storage/rnn-$t-50-1/result_known.txt 2>&1
-#         { python tagger.py --evaluate data/evaluation_unknown.txt -t $t -s 50 -n 1 ; } > storage/rnn-$t-50-1/result_unknown.txt 2>&1
-#         cp saved/* storage/rnn-$t-50-1/
-#         printf "Model with %i past words is saved and evaluated.\n" $t
-#         python tagger.py --reset -fq
-#     fi
-# done
+printf "\n## N Timesteps:\n"
+for t in {1..6}; do
+    if [ ! -d "storage/rnn-$t-50-1" ]; then
+        mkdir storage/rnn-$t-50-1
+        { time python tagger.py --train data/training.corpus -t $t -s 50 -n 1 ; } > storage/rnn-$t-50-1/training.txt 2>&1
+        { python tagger.py --evaluate data/evaluation_known.txt -t $t -s 50 -n 1 ; } > storage/rnn-$t-50-1/result_known.txt 2>&1
+        { python tagger.py --evaluate data/evaluation_unknown.txt -t $t -s 50 -n 1 ; } > storage/rnn-$t-50-1/result_unknown.txt 2>&1
+        cp saved/* storage/rnn-$t-50-1/
+        printf "Model with %i timesteps is saved and evaluated.\n" $t
+        python tagger.py --reset -fq
+    fi
+done
 
-# printf "\n## Hidden Layer Size:\n"
-# for s in 10 25 50 100; do
-#     if [ ! -d "storage/rnn-1-$s-1" ]; then
-#         mkdir storage/rnn-1-$s-1
-#         { time python tagger.py --train data/training.corpus -t 1 -s $s -n 1 ; } > storage/rnn-1-$s-1/training.txt 2>&1
-#         { python tagger.py --evaluate data/evaluation_known.txt -t 1 -s $s -n 1 ; } > storage/rnn-1-$s-1/result_known.txt 2>&1
-#         { python tagger.py --evaluate data/evaluation_unknown.txt -t 1 -s $s -n 1 ; } > storage/rnn-1-$s-1/result_unknown.txt 2>&1
-#         cp saved/* storage/rnn-1-$s-1/
-#         printf "Model with hidden layer size of %i is saved and evaluated.\n" $s
-#         python tagger.py --reset -fq
-#     fi
-# done
+printf "\n## Hidden Layer Size:\n"
+for s in 10 25 50 100; do
+    if [ ! -d "storage/rnn-1-$s-1" ]; then
+        mkdir storage/rnn-1-$s-1
+        { time python tagger.py --train data/training.corpus -t 1 -s $s -n 1 ; } > storage/rnn-1-$s-1/training.txt 2>&1
+        { python tagger.py --evaluate data/evaluation_known.txt -t 1 -s $s -n 1 ; } > storage/rnn-1-$s-1/result_known.txt 2>&1
+        { python tagger.py --evaluate data/evaluation_unknown.txt -t 1 -s $s -n 1 ; } > storage/rnn-1-$s-1/result_unknown.txt 2>&1
+        cp saved/* storage/rnn-1-$s-1/
+        printf "Model with hidden layer size of %i is saved and evaluated.\n" $s
+        python tagger.py --reset -fq
+    fi
+done
 
-# printf "\n## Training Epochs:\n"
-# for n in 1 5 10 20; do
-#     if [ ! -d "storage/rnn-1-50-$n" ]; then
-#         mkdir storage/rnn-1-50-$n
-#         { time python tagger.py --train data/training.corpus -t 1 -s 50 -n $n ; } > storage/rnn-1-50-$n/training.txt 2>&1
-#         { python tagger.py --evaluate data/evaluation_known.txt -t 1 -s 50 -n $n ; } > storage/rnn-1-50-$n/result_known.txt 2>&1
-#         { python tagger.py --evaluate data/evaluation_unknown.txt -t 1 -s 50 -n $n ; } > storage/rnn-1-50-$n/result_unknown.txt 2>&1
-#         cp saved/* storage/rnn-1-50-$n/
-#         printf "Model trained with %i epochs is saved and evaluated.\n" $n
-#         python tagger.py --reset -fq
-#     fi
-# done
+printf "\n## Training Epochs:\n"
+for n in 1 5 10 20; do
+    if [ ! -d "storage/rnn-1-50-$n" ]; then
+        mkdir storage/rnn-1-50-$n
+        { time python tagger.py --train data/training.corpus -t 1 -s 50 -n $n ; } > storage/rnn-1-50-$n/training.txt 2>&1
+        { python tagger.py --evaluate data/evaluation_known.txt -t 1 -s 50 -n $n ; } > storage/rnn-1-50-$n/result_known.txt 2>&1
+        { python tagger.py --evaluate data/evaluation_unknown.txt -t 1 -s 50 -n $n ; } > storage/rnn-1-50-$n/result_unknown.txt 2>&1
+        cp saved/* storage/rnn-1-50-$n/
+        printf "Model trained with %i epochs is saved and evaluated.\n" $n
+        python tagger.py --reset -fq
+    fi
+done
